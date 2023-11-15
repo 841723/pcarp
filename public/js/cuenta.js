312c3b07929e3c0cf9b4bc2d34648276f79c8c7a
846088romeo <146849583+846088romeo@users.noreply.github.com>
2023-11-15 09:39:38 +0100

    15/11/2023 9:39

--- public/js/cuenta.js
+++ public/js/cuenta.js
@@ -1,38 +1,97 @@
 function mostrarDatos() {
-    var zonaDatos = document.getElementById('datos-personales');
-    var zonaPedidos = document.getElementById('zona-pedidos');
+  var zonaDatos = document.getElementById('datos-personales');
+  var zonaPedidos = document.getElementById('zona-pedidos');
 
-    zonaDatos.style.display = 'block';
-    zonaPedidos.style.display = 'none';
+  zonaDatos.style.display = 'block';
+  zonaPedidos.style.display = 'none';
 }
 
 function mostrarPedidos() {
-    var zonaDatos = document.getElementById('datos-personales');
-    var zonaPedidos = document.getElementById('zona-pedidos');
+  var zonaDatos = document.getElementById('datos-personales');
+  var zonaPedidos = document.getElementById('zona-pedidos');
 
-    zonaDatos.style.display = 'none';
-    zonaPedidos.style.display = 'block';
+  zonaDatos.style.display = 'none';
+  zonaPedidos.style.display = 'block';
 }
 
-function pedirCuenta(){
+function mostrarContrasena() {
+  var x = document.getElementById("pass-save");
+  var icono = document.querySelector('.eye-icon');
 
+  if (x.type === "password") {
+      x.type = "text";
+      icono.classList.remove('bx-hide');
+      icono.classList.add('bx-show');
+  } else {
+      x.type = "password";
+      icono.classList.remove('bx-show');
+      icono.classList.add('bx-hide');
+  }
+}
+// Función para obtener y mostrar los pedidos del cliente
+function obtenerPedidosCliente() {
+  fetch('/api/pedidos') // Supongamos que el servidor tiene una API que devuelve los pedidos del cliente
+      .then(response => response.json())
+      .then(data => organizarPedidos(data))
+      .catch(error => console.error('Error al obtener pedidos:', error));
 }
 
-pwShowHide.forEach(eyeIcon => {
-    eyeIcon.addEventListener("click", () => {
-      let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
-      
-      pwFields.forEach(password => {
-          if(password.type === "password"){
-            password.type = "text";
-            eyeIcon.classList.replace("bx-hide", "bx-show");
-            return;
-          }
-          password.type = "password";
-          eyeIcon.classList.replace("bx-show", "bx-hide");
-      })
-    })
-  })  
+// Función para organizar y mostrar los pedidos en las secciones correspondientes
+function organizarPedidos(pedidos) {
+  const pedidosEnProceso = document.getElementById('pedidos-en-proceso');
+  const pedidosEnviados = document.getElementById('pedidos-enviados');
+  const pedidosEntregados = document.getElementById('pedidos-entregados');
+
+  // Limpiar las listas de pedidos antes de agregar nuevos elementos
+  pedidosEnProceso.innerHTML = '';
+  pedidosEnviados.innerHTML = '';
+  pedidosEntregados.innerHTML = '';
+
+  // Iterar sobre los pedidos y agregarlos a las listas correspondientes
+  pedidos.forEach(pedido => {
+      const li = document.createElement('li');
+      li.classList.add('pedido');
+
+      const pedidoInfo = document.createElement('div');
+      pedidoInfo.classList.add('pedido-info');
+
+      // Añadir atributos del pedido
+      const atributos = ['Número de Pedido', 'Fecha', 'Total', 'Estado']; // Modifica según tus atributos
+      atributos.forEach(atributo => {
+          const p = document.createElement('p');
+          p.innerHTML = `<strong>${atributo}:</strong> ${pedido[atributo.toLowerCase()]}`;
+          pedidoInfo.appendChild(p);
+      });
+
+      li.appendChild(pedidoInfo);
+
+      // Añadir botón de cancelar para pedidos solicitados
+      if (pedido.estado === 'SOLICITADO') {
+          const cancelarBtn = document.createElement('button');
+          cancelarBtn.textContent = 'Cancelar Pedido';
+          cancelarBtn.addEventListener('click', () => cancelarPedido(pedido.numeroPedido));
+          li.appendChild(cancelarBtn);
+      }
+
+      // Agregar el elemento a la lista correspondiente según el estado del pedido
+      if (pedido.estado === 'SOLICITADO') {
+          pedidosEnProceso.appendChild(li);
+      } else if (pedido.estado === 'ENVIADO') {
+          pedidosEnviados.appendChild(li);
+      } else if (pedido.estado === 'ENTREGADO') {
+          pedidosEntregados.appendChild(li);
+      }
+  });
+}
+
+// Función para cancelar un pedido solicitado
+function cancelarPedido(numeroPedido) {
+  // Lógica para cancelar el pedido
+  console.log(`Cancelar pedido #${numeroPedido}`);
+}
+
+// Llamar a la función para obtener y mostrar los pedidos del cliente al cargar la página
+document.addEventListener('DOMContentLoaded', obtenerPedidosCliente);
 
 // Mostrar la sección de "Mis Datos" por defecto al cargar la página
 mostrarDatos();
